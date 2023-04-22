ed820140c369bee4fca31beee50f685b278157c5
Cotelea <carlo.cotelea@ugs.ro>
2023-04-22 17:27:24 +0300

    added flatshade and boundbox buttons

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { Viewer } from "./lib/ViewerThree";
 
 var domdiv = document.getElementById("viewer")! as HTMLDivElement;
 var buttonWireframe = document.getElementById("wireframe-button")! as HTMLInputElement;
+var buttonFlatShade = document.getElementById("flatshade-button")! as HTMLInputElement;
+var buttonBoundBox = document.getElementById("boundbox-button")! as HTMLInputElement;
 var buttonResetRotation = document.getElementById("reset-rot")! as HTMLButtonElement;
 var buttonResetPosition = document.getElementById("reset-pos")! as HTMLButtonElement;
 var buttonResetZoom = document.getElementById("reset-zoom")! as HTMLButtonElement;
@@ -10,9 +12,13 @@ var buttonResetZoom = document.getElementById("reset-zoom")! as HTMLButtonElemen
 var buttonsSetup = () => {
 	/// Setting values
 	buttonWireframe.checked = test.wireframe;
+	buttonFlatShade.checked = test.flatShade;
+	buttonBoundBox.checked = test.boundBox;
 
 	/// Adding events
 	buttonWireframe.addEventListener("click", () => { test.wireframe = buttonWireframe.checked });
+	buttonFlatShade.addEventListener("click", () => { test.flatShade = buttonFlatShade.checked });
+	buttonBoundBox.addEventListener("click", () => { test.boundBox = buttonBoundBox.checked });
 	buttonResetRotation.addEventListener("click", () => { test.ResetRotation() });
 	buttonResetPosition.addEventListener("click", () => { test.ResetPosition() });
 	buttonResetZoom.addEventListener("click", () => { test.ResetZoom() });
@@ -26,7 +32,9 @@ var test = new Viewer({
 			focusDisplay: false,
 		},
 		object: {
-			wireframe: true,
+			wireframe: false,
+			flatShade: false,
+			boundBox: false
 		}
 	},
 	onLoad: buttonsSetup
